54e68f9f305c27d14284746d0381b2ff95996c62
Jan-Hendrik Zab <zab@l3s.de>
2015-08-05 16:21:12 +0200

    Added some preliminary parboiled2 parsing code

--- src/main/scala/upsc.scala
+++ src/main/scala/upsc.scala
@@ -3,6 +3,7 @@ import scala.util.matching._
 import scala.sys.process._
 import scalaz._
 import Scalaz._
+import org.parboiled2._
 
 case class Config(dev: String = "", check: Map[String, String] = Map())
 
@@ -35,7 +36,7 @@ object UPSC {
       "upsc binary doesn't exist.".left
   }
 
-  def getValueFromParam(param: String): \/[String, Double] = {
+  def getValueFromLine(param: String): \/[String, Double] = {
     val Regex = """[\w\.]+:\s+(\d+.*)""".r
     param match {
       case Regex(v) => v.toDouble.right
@@ -43,19 +44,29 @@ object UPSC {
     }
   }
 
-  def getThresholds(param: String, k: String, v: String): \/[String, (String, (Double, Int))] = {
-    val Regex = """w:(\d+)&c:(\d+)""".r
-    v match {
-      case Regex(w, c) => getValueFromParam(param) map ( v => v match {
-        case i if i > c.toDouble => (k, (i, 2))
-        case i if i > w.toDouble => (k, (i, 1))
-        case i => (k, (i, 0))
-      } )
-      case _ => s"Couldn't extract thresholds for $k = $v".left
-    }
+  def toEither(t: scala.util.Try[List[Check]]): \/[String, List[Check]] = t match {
+    case scala.util.Success(v) => v.right
+    case scala.util.Failure(v) => v.getMessage.left
   }
 
-  def parseOutput(output: Array[String], config: Config): \/[String, Map[String, (Double, Int)]] = {
+  def calculateThreshold(l: List[Check], value: Double): (String, (Double, Long)) = {
+    val errorLevel = l.map(check =>
+      math.max(check.comp(value, check.w) match { case true => 0l case false => 1l},
+        check.comp(value, check.c) match { case true => 0l case false => 2l})).max
+
+    errorLevel
+  }
+
+  def getThresholds(line: String, param: String, c: String): \/[String, (String, (Double, Long))] = {
+    for {
+      value <- getValueFromLine(line)
+      checks <- toEither(new ThresholdParser(c).InputLine.run())
+    } yield calculateThreshold(checks, value)
+
+//      case _ => s"Couldn't extract thresholds for $k = $v".left
+  }
+
+  def parseOutput(output: Array[String], config: Config): \/[String, Map[String, (Double, Long)]] = {
     val params = config.check.map{
       case (k,v) => output.find(_.startsWith(k)).\/>(s"Could not find $k in upsc output.") >>= { getThresholds(_,k,v) }
     }
@@ -66,7 +77,7 @@ object UPSC {
       params.collect{ case \/-(v) => v }.toMap.right
   }
 
-  def createReadableOutput(p: Map[String, (Double, Int)], level: Int): String = {
+  def createReadableOutput(p: Map[String, (Double, Long)], level: Long): String = {
     val msgStart = level match {
       case 3 => "UNKNOWN: "
       case 2 => "CRITICAL: "
@@ -78,11 +89,13 @@ object UPSC {
   }
 
   // TODO: add warn:crit values to perfdata
-  def createPerfdataOutput(p: Map[String, (Double, Int)]): String = "| " + p.map{ case (k,v) => s"${k}=${v._1} "}.foldLeft("")(_ + _)
+  def createPerfdataOutput(p: Map[String, (Double, Long)]): String = "| " + p.map{ case (k,v) => s"${k}=${v._1} "}.foldLeft("")(_ + _)
 
-  def getLevel(p: Map[String, (Double, Int)]): \/[String, Int] = p.maxBy(_._2._2)._2._2.right
+  def getLevel(p: Map[String, (Double, Long)]): \/[String, Long] = p.maxBy(_._2._2)._2._2.right
 
   def main(args: Array[String]): Unit = {
+    val p = new ThresholdParser("(>,80.5,90),(<,30,10)").InputLine.run()
+    println(p)
     val output = for {
       config <- parseConfig(args)
       output <- getUpscOutput(config.dev)
@@ -91,7 +104,7 @@ object UPSC {
     } yield (createReadableOutput(parsed, level) + createPerfdataOutput(parsed), level)
 
     output match {
-      case \/-((output,level)) => println(output); sys.exit(level)
+      case \/-((output,level)) => println(output); sys.exit(level.toInt)
       case -\/(msg) => println(msg); sys.exit(3)
     }
   }
