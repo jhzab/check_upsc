97df345180a1cc20d0b1aa38faaa220a62562156
Jan-Hendrik Zab <zab@l3s.de>
2015-08-05 10:56:48 +0200

    Changed call syntax to work around Nagios killing anything after a ';'

--- src/main/scala/upsc.scala
+++ src/main/scala/upsc.scala
@@ -15,7 +15,7 @@ object UPSC {
       opt[String]('d', "dev") required() action { (x, c) =>
         c.copy(dev = x) } text("dev is a required parameter, ie. USER@HOSTNAME")
       opt[Map[String, String]]('c', "check") required() valueName("k1=v1,k2=v2...") action { (x, c) =>
-        c.copy(check = x) } text("This specifies what to check and when to give a warning or critical message, ie. ups.load=w:80;c:90")
+        c.copy(check = x) } text("This specifies what to check and when to give a warning or critical message, ie. ups.load=w:80&c:90")
       help("help") text("prints this usage text")
       note("\nBoth --check and --dev need to be defined!")
     }
@@ -44,7 +44,7 @@ object UPSC {
   }
 
   def getThresholds(param: String, k: String, v: String): \/[String, (String, (Double, Int))] = {
-    val Regex = """w:(\d+);c:(\d+)""".r
+    val Regex = """w:(\d+)&c:(\d+)""".r
     v match {
       case Regex(w, c) => getValueFromParam(param) map ( v => v match {
         case i if i > c.toDouble => (k, (i, 2))
